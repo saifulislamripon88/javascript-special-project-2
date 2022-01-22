0a92c2573b469a07c09a1f6d28ac8edf233bfa78
saifulislamripon <si3491061@gmail.com>
2022-01-23 01:45:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// only text part animation korar code
+// gsap.to('.text', {y:100, opacity:0, duration: 18})
+
+//only imagee animation korar code
+// gsap.to('.imagess',{y:150, duration: 10})
+
+
+gsap.fromTo('.text',{opacity:0,y:20},{opacity: 1 ,y: 0, duration:6});
